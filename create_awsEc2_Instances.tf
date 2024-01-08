be8d87a850ade7c120d666e682893c0ef4f5c1fa
Rahul Kumar <95223485+RahulSinha9@users.noreply.github.com>
2024-01-08 22:14:56 +0530

    Add files via upload

---
+++ create_awsEc2_Instances.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "jack.roper"
+}
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-04e914639d0cca79a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "JacksBlogExample"
+  }
+}
