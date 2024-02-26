812be8c40923271482a757886b00a329e3c74504
Rahul Kumar <95223485+RahulSinha9@users.noreply.github.com>
2024-02-26 18:34:21 +0530

    Add files via upload

---
+++ create_local_file.tf
@@ -0,0 +1,4 @@
+resource "local_file" "devops"{
+        filename="/home/ubuntu/Terraform/Terraform-local/devops-automated.txt"
+        content= "I want to become a DevOps Engineer who knows Terraform"
+}
