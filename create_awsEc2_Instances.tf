15dacf8b71073b865b8a4d983fa57e00ce39f873
Rahul Kumar <95223485+RahulSinha9@users.noreply.github.com>
2024-02-28 22:15:48 +0530

    Update create_awsEc2_Instances.tf

--- create_awsEc2_Instances.tf
+++ create_awsEc2_Instances.tf
@@ -1,24 +1,22 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+        required_providers{
+        aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
 
-  required_version = ">= 1.2.0"
 }
-
+}
+required_version = ">=1.2.0"
+}
 provider "aws" {
-  region  = "us-west-2"
-  profile = "rahul.sinha"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example_server" {
-  ami           = "ami-04e914639d0cca79a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "DevOps"
-  }
+resource "aws_instance" "my_ec2_instance"{ 
+   ami="ami-0c7217cdde317cfec"
+   count = 5
+   instance_type="t2.micro"
+   tags = {
+          name = "Terraform-Test"
+}
 }
